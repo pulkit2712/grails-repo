20555fd6506b22bc4f1a7a00fa180a8442eab045
Pulkit <pulkit2712@gmail.com>
2021-06-28 23:40:50 +0530

    adding Service test

--- myapp/src/test/groovy/myapp/CsvContentServiceSpec.groovy
+++ myapp/src/test/groovy/myapp/CsvContentServiceSpec.groovy
@@ -4,27 +4,42 @@ import com.uberall.locations.services.LocationImpl
 import grails.testing.mixin.integration.Integration
 import grails.testing.services.ServiceUnitTest
 import grails.web.servlet.mvc.GrailsParameterMap
+import org.grails.datastore.gorm.bootstrap.support.InstanceFactoryBean
 import org.grails.plugins.testing.GrailsMockHttpServletRequest
 import spock.lang.Specification
+
+import java.util.stream.Collectors
+
 @Integration(applicationClass = Application.class)
-class JsonContentServiceSpec extends Specification implements ServiceUnitTest<JsonContentService>{
+class CsvContentServiceSpec extends Specification implements ServiceUnitTest<CsvContentService>{
     def locationImpl
-    JsonContentService c
+    CsvContentService c
     static doWithSpring = {
         locationImpl(LocationImpl)
     }
+
     def setup() {
-        c=new JsonContentService()
+        c=new CsvContentService()
         c.locationImpl=locationImpl
+        // locationImpl = applicationContext.getBean("locationImpl")
     }
 
     def cleanup() {
     }
 
+    void "test content"() {
+
+
+        def s=c.getContentType()
+
+
+        expect:"fix me"
+        s.equals("text/csv;charset=UTF-8")
+    }
     void "test data"() {
         GrailsMockHttpServletRequest request = new GrailsMockHttpServletRequest()
         request.setParameter("size","50")
-        request.setParameter("content","json")
+        request.setParameter("content","csv")
 
 
         def params=new GrailsParameterMap([:], request)
--- myapp/src/test/groovy/myapp/XmlContentServiceSpec.groovy
+++ myapp/src/test/groovy/myapp/XmlContentServiceSpec.groovy
@@ -1,18 +1,37 @@
 package myapp
 
+import com.uberall.locations.services.LocationImpl
+import grails.testing.mixin.integration.Integration
 import grails.testing.services.ServiceUnitTest
+import grails.web.servlet.mvc.GrailsParameterMap
+import org.grails.plugins.testing.GrailsMockHttpServletRequest
 import spock.lang.Specification
-
+@Integration(applicationClass = Application.class)
 class XmlContentServiceSpec extends Specification implements ServiceUnitTest<XmlContentService>{
-
+    def locationImpl
+    XmlContentService c
+    static doWithSpring = {
+        locationImpl(LocationImpl)
+    }
     def setup() {
+        c=new XmlContentService()
+        c.locationImpl=locationImpl
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "test data"() {
+        GrailsMockHttpServletRequest request = new GrailsMockHttpServletRequest()
+        request.setParameter("size","50")
+        request.setParameter("content","xml")
+
+
+        def params=new GrailsParameterMap([:], request)
+        def lines=c.getData(params)
+
+
+        expect:
+        lines!=null
     }
 }
